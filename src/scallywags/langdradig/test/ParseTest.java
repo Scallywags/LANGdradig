ede7361fc2f6fb93b6b93635082ed3d846d2fa80
Jannyboy11 <jannyboy11@gmail.com>
2016-06-13 15:15:26 +0200

    test example 1

--- src/scallywags/langdradig/test/ParseTest.java
+++ src/scallywags/langdradig/test/ParseTest.java
@@ -14,13 +14,18 @@ import scallywags.langdradig.grammatica.LANGdradigParser;
 public class ParseTest {
 	
 	@Test
-	public void test0()throws Exception {
-		ANTLRInputStream stream = new ANTLRFileStream("src/scallywags/langdradig/example/test0.langdradig");
+	public void test()throws Exception {
+		test("src/scallywags/langdradig/example/test0.langdradig");
+		test("src/scallywags/langdradig/example/test1.langdradig");
+	}
+	
+	private void test(String path) throws Exception {
+		ANTLRInputStream stream = new ANTLRFileStream(path);
 		Lexer lexer = new LANGdradigLexer(stream);
 		TokenStream tokens = new CommonTokenStream(lexer);
 		LANGdradigParser parser = new LANGdradigParser(tokens);
 		ParseTree tree = parser.program();
-		System.out.println(tree.toStringTree(parser));
-	}	
+		System.out.println(tree.toStringTree(parser));	//TODO actually test branches
+	}
 
 }
