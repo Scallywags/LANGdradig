841938ea20f225e68a7a5f028b6b7aa8bc6af11c
Jeroen W <jweener1996@gmail.com>
2016-06-18 10:41:23 +0200

    Some ide changes

--- src/scallywags/langdradig/ide/frames/Main.java
+++ src/scallywags/langdradig/ide/frames/Main.java
@@ -7,6 +7,8 @@ import scallywags.langdradig.ide.errors.LANGdradigError;
 import scallywags.langdradig.ide.errors.LANGdradigErrorBuilder;
 
 import javax.swing.*;
+import javax.swing.filechooser.*;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
@@ -26,12 +28,15 @@ import java.util.List;
 // TODO translate certain errors
 // TODO verwacht onbekend should be undeclared error
 // TODO Fix bug in checker -> b is een waarhseid <- gaat stuk met een nullpointer
+// TODO opslaan bestaan met extensie
 
 public class Main extends JFrame {
     private static final String EXTENSION = ".langdradig";
     private JPanel contentPane;
     private JButton openButton;
     private JButton saveButton;
+
+    private final JFileChooser fc = new JFileChooser();
 
     private JTextArea codeArea;
     private Highlighter highlighter;
@@ -46,14 +51,22 @@ public class Main extends JFrame {
     private JPanel messagesPanel;
     private JButton startButton;
     private JScrollPane codeScrollPane;
+    private JButton newButton;
 
     private String filePath;
+
+    private boolean madeChanges;
 
     public Main() {
         setContentPane(contentPane);
         getRootPane().setDefaultButton(openButton);
 
+        madeChanges = false;
+
         codeArea.setTabSize(2);
+        setUpKeyListener();
+
+        newButton.addActionListener(e -> onNew());
 
         openButton.addActionListener(e -> onOpen());
 
@@ -72,6 +85,18 @@ public class Main extends JFrame {
         painter = new DefaultHighlighter.DefaultHighlightPainter(Color.PINK);
         highlightTags = new HashMap<>();
 
+        fc.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                return f.isDirectory() || f.getName().endsWith(".langdradig");
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+        });
+
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
@@ -85,25 +110,44 @@ public class Main extends JFrame {
     }
 
     private void onOpen() {
-        final JFileChooser fc = new JFileChooser();
+        if (madeChanges) {
+            SaveDialog s = new SaveDialog(this);
+            switch (s.getDecision()) {
+                case OK:
+                    onSave();
+                    break;
+                case CANCEL:
+                    return;
+                case NO:
+                    // Nothing
+                    break;
+                default:
+                    break;
+            }
+        }
         int returnVal = fc.showOpenDialog(this);
 
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File file = fc.getSelectedFile();
             String fileName = file.getName();
-            if (fileName.endsWith(EXTENSION)) {
-                filePath = file.getAbsolutePath();
-                this.setTitle("LANGdradig IDE - " + fileName);
-                try {
-                    String content = new String(Files.readAllBytes(file.toPath()));
-                    codeArea.setText(content);
-                    codeArea.setEditable(true);
-                } catch (IOException e) {
-                    // No permission to read ? Ignore
-                    e.printStackTrace();
+            if (file.exists()) {
+                if (fileName.endsWith(EXTENSION)) {
+                    filePath = file.getAbsolutePath();
+                    this.setTitle("LANGdradig IDE - " + fileName);
+                    try {
+                        String content = new String(Files.readAllBytes(file.toPath()));
+                        codeArea.setText(content);
+                        onSave();
+                        setUpKeyListener();
+                    } catch (IOException e) {
+                        // No permission to read ? Ignore
+                        e.printStackTrace();
+                    }
+                } else {
+                    new ErrorDialog("Bestandstype niet ondersteunt", "Open een bestand met de extensie .langdradig");
                 }
             } else {
-                new ErrorDialog("Bestandstype niet ondersteunt", "Open een bestand met de extensie .langdradig");
+                new ErrorDialog("Niet gevonden", "Het geselecteerde betand kan niet worden gevonden.");
             }
         }
     }
@@ -112,9 +156,49 @@ public class Main extends JFrame {
         dispose();
     }
 
-    private void onSave() {
+    private void onNew() {
+        if (madeChanges) {
+            SaveDialog s = new SaveDialog(this);
+            switch (s.getDecision()) {
+                case OK:
+                    onSave();
+                    break;
+                case CANCEL:
+                    return;
+                case NO:
+                    break;
+                default:
+                    break;
+            }
+        }
+        reset();
+    }
+
+    public void reset() {
+        setTitle("LANGdradig IDE - Nieuw Bestand");
+        messagesArea.setText("");
+        messagesArea.setBackground(Color.WHITE);
+        filePath = null;
+
+        madeChanges = false;
+        codeArea.setText("");
+        setUpKeyListener();
+    }
+
+    public void onSave() {
         String content = codeArea.getText();
         highlighter.removeAllHighlights();
+
+        if (filePath == null) {
+            int returnValue = fc.showSaveDialog(this);
+            if (returnValue != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
+            File f = fc.getSelectedFile();
+            filePath = f.getAbsolutePath();
+            this.setTitle("LANGdradig IDE - " + f.getName());
+        }
+
         try (PrintWriter writer = new PrintWriter(filePath, "UTF-8")) {
             writer.write(content);
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
@@ -122,6 +206,8 @@ public class Main extends JFrame {
         }
         clearMessages();
         checkContent();
+
+        madeChanges = false;
     }
 
     private void onStart() {
@@ -129,7 +215,6 @@ public class Main extends JFrame {
     }
 
     private void checkContent() {
-        print("Opslaan...");
         Checker checker = new Checker();
         checker.checkString(codeArea.getText());
         if (checker.getCheckerExceptions().isEmpty() && checker.getParserExceptions().isEmpty()) {
@@ -147,8 +232,19 @@ public class Main extends JFrame {
         }
     }
 
+    private void setUpKeyListener() {
+        codeArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                madeChanges = true;
+                codeArea.removeKeyListener(this);
+            }
+        });
+    }
+
     public void clearMessages() {
         messagesArea.setText("");
+        messagesArea.setBackground(Color.WHITE);
     }
 
     private void toggleMessages() {
@@ -185,9 +281,13 @@ public class Main extends JFrame {
     public static void main(String[] args) {
         Main dialog = new Main();
         dialog.pack();
-        dialog.setTitle("LANGdradig IDE");
+        dialog.setTitle("LANGdradig IDE - Nieuw Bestand");
         dialog.setResizable(true);
         dialog.setExtendedState(JFrame.MAXIMIZED_BOTH);
         dialog.setVisible(true);
+    }
+
+    public String getFilePath() {
+        return filePath;
     }
 }
