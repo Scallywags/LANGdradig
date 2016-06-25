e67e3d48ec33830809b825b36710565fd92d2e15
Jeroen W <jweener1996@gmail.com>
2016-06-25 20:13:15 +0200

    Started working on the syntaxhighlighter

--- src/scallywags/langdradig/ide/features/unfinished/SyntaxHighlighter.java
+++ src/scallywags/langdradig/ide/features/unfinished/SyntaxHighlighter.java
@@ -3,6 +3,11 @@ package scallywags.langdradig.ide.features.unfinished;
 import scallywags.langdradig.generate.Variable;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -14,5 +19,28 @@ public class SyntaxHighlighter {
     public static void highlightVariables(JTextArea area, List<Variable> variables) {
         if (!ON) return;
         //TODO
+    }
+
+    public static void colorKeywords(JTextPane area, String content) {
+        try {
+            SimpleAttributeSet set = new SimpleAttributeSet();
+            StyleConstants.setItalic(set, true);
+            StyleConstants.setForeground(set, Color.YELLOW);
+            String searchString = content;
+            Document doc = area.getStyledDocument();
+            int start;
+            int acc = 0;
+            do {
+                start = searchString.indexOf("als");
+                acc = start + searchString.indexOf("als");
+                doc.insertString(start, "SLA", set);
+                doc.remove(start + 3, 3);
+                searchString = searchString.substring(acc);
+                System.out.println("TEST");
+            } while (start != -1);
+            area.setCharacterAttributes(new SimpleAttributeSet(), true);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
     }
 }
