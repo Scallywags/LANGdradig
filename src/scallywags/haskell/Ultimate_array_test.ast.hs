7842512027577d25bfac7a0f470bce95d05e45ea
Jannyboy11 <jannyboy11@gmail.com>
2016-06-30 16:25:04 +0200

    fix simulation

---
+++ src/scallywags/haskell/Ultimate_array_test.ast.hs
@@ -0,0 +1,14 @@
+import AST
+import Generator
+
+ast :: Prog
+ast =  Prog (Decl "a" (ArrayType 5 IntType):Decl "b" (ArrayType 5 IntType):Expr (Ass "a" ((Array ((Int 5):(Int 4):(Int 3):(Int 2):(Int 1):[])))):Expr (Ass "b" ((Array ((Int 5):(Int 4):(Int 3):(Int 2):(Int 1):[])))):Decl "c" (BoolType):Expr (Ass "c" (BinOp Equal ((Idf "a")) ((Idf "b")))):Print ((Idf "c")) (BoolType):[]) 
+
+main :: IO ()
+main = writeFile "Ultimate_array_test.spril.hs" text where
+    text =  "import HardwareTypes\nimport Simulation\n\n" ++
+            "prog :: [Instruction]\n" ++
+            "prog = " ++ show instructions ++ "\n\n" ++ --RIP no optimizer :(
+            "main :: IO ()\n" ++ 
+            "main = sysRun $ replicate " ++ show (length (t_ids state) + 1) ++ " prog"
+                where (instructions, state) = generate ast
