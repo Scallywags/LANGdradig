1f2e189ba04e687ea4e93d797a674ca790f78538
Jannyboy11 <jannyboy11@gmail.com>
2016-07-01 14:27:57 +0200

    test inverted expressions

--- src/scallywags/haskell/Expressions.ast.hs
+++ src/scallywags/haskell/Expressions.ast.hs
@@ -2,7 +2,7 @@ import AST
 import Generator
 
 ast :: Prog
-ast =  Prog (DeclShared "b" (BoolType):Expr (Ass "b" (BinOp GreaterThan (BinOp Plus ((Int 5)) (BinOp Times ((Int 3)) (BinOp Power ((Int 2)) ((Int 4))))) (BinOp Times ((Par (BinOp Plus ((Int 2)) ((Int 3))))) ((Par (BinOp Plus ((Int 2)) (BinOp Power ((Int 2)) ((Int 3))))))))):Print ((Idf "b")) (BoolType):[]) 
+ast =  Prog (DeclShared "b" (BoolType):Expr (Ass "b" (BinOp GreaterThan (BinOp Plus ((Int 5)) (BinOp Times ((Int 3)) (BinOp Power ((Int 2)) ((Int 4))))) (BinOp Times ((Par (BinOp Plus ((Int 2)) ((Int 3))))) ((Par (BinOp Plus ((Int 2)) (BinOp Power ((Int 2)) ((Int 3))))))))):IfThenElse (UnOp Not ((Idf "b"))) (Block (Print (BinOp Plus ((Int 5)) (BinOp Times ((Int 3)) (BinOp Power ((Int 2)) ((Int 4))))) (IntType):Print (BinOp Times ((Par (BinOp Plus ((Int 2)) ((Int 3))))) ((Par (BinOp Plus ((Int 2)) (BinOp Power ((Int 2)) ((Int 3))))))) (IntType):[])) (Block (Print (BinOp Times ((Par (BinOp Plus ((Int 2)) ((Int 3))))) ((Par (BinOp Plus ((Int 2)) (BinOp Power ((Int 2)) ((Int 3))))))) (IntType):Print (BinOp Plus ((Int 5)) (BinOp Times ((Int 3)) (BinOp Power ((Int 2)) ((Int 4))))) (IntType):[])):[]) 
 
 main :: IO ()
 main = writeFile "Expressions.spril.hs" text where
